6d143d9040f48f4bf21fddfb0140b0cdf3724110
CryoSolace <100377930+CryoSolace@users.noreply.github.com>
2023-01-22 12:55:46 +0000

    Added docstrings to some functions

---
+++ functions/isPrimeBy6k.py
@@ -0,0 +1,10 @@
+from math import sqrt
+
+def primeBy6k(n, showSteps = False):
+    step = 1
+    while n % 6*step - 1 < sqrt(n): # looks broken, needs fixing, consult wikipedia
+        if n % 6*step - 1 == 0:
+            print(n)
+        elif n % 6*step + 1 == 0:
+            print(n)
+        step += 1
